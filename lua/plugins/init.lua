cac805a35f452c0214f130d98a493da085364129
Ryan Verbrugge <ryan.verbrugge@gosteelhead.com>
2025-09-30 10:07:50 -0400

    fixed plugin issues

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,6 +5,53 @@ return {
     opts = require "configs.conform",
   },
 
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    opts = {
+      ui = { border = "rounded" },
+    },
+  },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    opts = {
+      ensure_installed = { "html", "cssls" },
+      automatic_installation = true,
+    },
+  },
+
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    opts = {
+      ensure_installed = { "stylua" },
+    },
+    config = function(_, opts)
+      require("mason-tool-installer").setup(opts)
+
+      vim.api.nvim_create_user_command("MasonInstallAll", function()
+        pcall(require("mason-lspconfig").setup, {
+          ensure_installed = { "html", "cssls" },
+          automatic_installation = true,
+        })
+
+        pcall(require("mason-tool-installer").install)
+      end, { desc = "Install all Mason LSPs and tools" })
+    end,
+  },
+  {
+      "toppair/peek.nvim",
+      event = { "VeryLazy" },
+      build = "deno task --quiet build:fast",
+      config = function()
+          require("peek").setup()
+          vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+          vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+      end,
+  },
+
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
@@ -26,3 +73,4 @@ return {
   -- 	},
   -- },
 }
+
