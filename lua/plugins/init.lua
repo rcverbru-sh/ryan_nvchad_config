e4b52d21bf09f8521d8f2b34870756cbace318d0
Ryan Verbrugge <ryan.verbrugge@gosteelhead.com>
2025-10-21 09:27:40 -0400

    adding prettier and some other stuff

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -38,6 +38,134 @@ return {
     end,
   },
 
+  -- Prettier formatting
+  {
+    "MunifTanjim/prettier.nvim",
+    event = "VeryLazy", -- Load when needed
+    config = function()
+      local prettier = require("prettier")
+      
+      prettier.setup({
+        bin = 'prettier', -- or `'prettierd'` (v0.23.3+)
+        filetypes = {
+          "css",
+          "graphql",
+          "html",
+          "javascript",
+          "javascriptreact",
+          "json",
+          "less",
+          "markdown",
+          "scss",
+          "typescript",
+          "typescriptreact",
+          "yaml",
+        },
+        cli_options = {
+          arrow_parens = "always",
+          bracket_spacing = true,
+          bracket_same_line = false,
+          embedded_language_formatting = "auto",
+          end_of_line = "lf",
+          html_whitespace_sensitivity = "css",
+          jsx_single_quote = false,
+          print_width = 80,
+          prose_wrap = "preserve",
+          quote_props = "as-needed",
+          semi = true,
+          single_attribute_per_line = false,
+          single_quote = false,
+          tab_width = 2,
+          trailing_comma = "es5",
+          use_tabs = false,
+          vue_indent_script_and_style = false,
+        },
+      })
+      
+      -- Set a global variable to indicate Prettier is loaded
+      vim.g.prettier_loaded = true
+    end,
+  },
+
+  -- Auto-pairs for brackets, quotes, etc.
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      local autopairs = require("nvim-autopairs")
+      
+      autopairs.setup({
+        check_ts = true, -- Enable treesitter integration
+        ts_config = {
+          lua = { "string", "source" },
+          javascript = { "string", "template_string" },
+          java = false,
+        },
+        disable_filetype = { "TelescopePrompt", "spectre_panel" },
+        fast_wrap = {
+          map = "<M-e>",
+          chars = { "{", "[", "(", '"', "'" },
+          pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+          offset = 0, -- Offset from pattern match
+          end_key = "$",
+          keys = "qwertyuiopzxcvbnmasdfghjkl",
+          check_comma = true,
+          highlight = "PmenuSel",
+          highlight_grey = "LineNr",
+        },
+      })
+
+      -- Basic autopairs functionality without cmp integration
+      -- This will work with blink.cmp automatically
+    end,
+  },
+
+  -- HTML/JSX tag auto-completion
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+    dependencies = { "nvim-treesitter" },
+    config = function()
+      require("nvim-ts-autotag").setup({
+        filetypes = {
+          "html",
+          "javascript",
+          "typescript",
+          "javascriptreact",
+          "typescriptreact",
+          "svelte",
+          "vue",
+          "tsx",
+          "jsx",
+          "rescript",
+          "xml",
+          "php",
+          "markdown",
+          "astro",
+          "glimmer",
+          "handlebars",
+          "hbs",
+        },
+        skip_tags = {
+          "area",
+          "base",
+          "br",
+          "col",
+          "embed",
+          "hr",
+          "img",
+          "input",
+          "link",
+          "meta",
+          "param",
+          "source",
+          "track",
+          "wbr",
+        },
+      })
+    end,
+  },
+
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     dependencies = { "williamboman/mason.nvim" },
@@ -162,6 +290,10 @@ return {
           vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<CR>", true, false, true), "n")
         end
       end, { desc = "Accept completion or insert newline" })
+      
+      -- Prettier keymaps
+      keymap("n", "<leader>f", "<Plug>(prettier-format)", { desc = "Format with Prettier" })
+      keymap("x", "<leader>f", "<Plug>(prettier-format)", { desc = "Format selection with Prettier" })
     end,
   },
 
